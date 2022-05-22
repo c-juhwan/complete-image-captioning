6301e7edf8c7203a6f249eb5e2951b31a321107d
Choi Juhwan <gold32317@gmail.com>
2022-05-22 10:55:43 +0000

    download test data

--- download.sh
+++ download.sh
@@ -11,6 +11,7 @@ mkdir dataset
 wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip -P ./dataset/
 wget http://images.cocodataset.org/zips/train2017.zip -P ./dataset/
 wget http://images.cocodataset.org/zips/val2017.zip -P ./dataset/
+wget http://images.cocodataset.org/zips/test2017.zip -P ./dataset/
 
 # Unzip COCO dataset
 unzip ./dataset/annotations_trainval2017.zip -d ./dataset/
@@ -19,3 +20,5 @@ unzip ./dataset/train2017.zip -d ./dataset/
 rm ./dataset/train2017.zip 
 unzip ./dataset/val2017.zip -d ./dataset/ 
 rm ./dataset/val2017.zip 
+unzip ./dataset/test2017.zip -d ./dataset/ 
+rm ./dataset/test2017.zip
